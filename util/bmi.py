39acc514574d67bb7ed887912aac2b82aff741d7
COitpark <minhobag18@gmail.com>
2022-10-24 18:10:25 +0900

    10-24

--- util/bmi.py
+++ util/bmi.py
@@ -1,53 +1,24 @@
-"""
-키와 몸무게를 입력받아서 비만도를 측정하는 프로그램을 완성하시오.
-BMI 지수를 구하는 공식은 다음과 같다.
-BMI지수 = 몸무게(70kg) / (키(1.7m) * 키(1.7m))
-BMI 지수에 따른 결과는 다음과 같다.
-고도 비만 : 35 이상
-중(重)도 비만 (2단계 비만) : 30 - 34.9
-경도 비만 (1단계 비만) : 25 - 29.9
-과체중 : 23 - 24.9
-정상 : 18.5 - 22.9
-저체중 : 18.5 미만
-해당값을 입력받으면 다음과 같이 출력되도록 하시오.
-***************************
-이름 키(cm) 몸무게(kg) 비만도
-***************************
-홍길동 170 79 정상
-***************************
-if answer >= 35 :
-            biman = "고도비만"
-        else :
-            biman = "점"
-"""
+
 class Bmi(object):
     def __init__(self, name, cm, kg):
-        self.biman = self.get_biman()
         self.name = name
         self.cm = cm
         self.kg = kg
-
-    def execute(self):
-        self.get_biman()
-        self.print_biman()
 
     def get_bmi(self):
         kg = self.kg
         m = self.cm / 100
         return kg / m ** 2
 
-    def get_biman(self):
+    def print_biman(self):
         biman = ""
         bmi = self.get_bmi()
-        if(bmi >= 35) : biman = "고도 비만"
-        elif(bmi >= 25) : biman ="중도 비만"
+        if(bmi >= 35) : biman = "고도비만"
+        elif(bmi >= 25) : biman ="중도비만"
         elif(bmi >= 23) : biman = "과체중"
         elif(bmi >= 18.5) : biman = "정상"
         else: biman = "저체중"
-        self.biman = biman
-
-    def print_biman(self):
-        print(f"{self.name} {self.cm} {self.kg} {self.biman} ")
+        print(f"{self.name} {self.cm} {self.kg} {biman} ")
 
 
     @staticmethod
