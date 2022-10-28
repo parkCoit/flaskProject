dd8338fc8242761151c71f44109dfd30d15a168e
COitpark <minhobag18@gmail.com>
2022-10-28 16:54:47 +0900

    10-28

--- titanic/models.py
+++ titanic/models.py
@@ -1,10 +1,19 @@
 import pandas as pd
 
+
 from util.dataset import Dataset
+
 """
  ['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp',
  'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
- === null 값 ===
+ 시각화를 통해 얻은 상관관계 변수(variable = feature = column)는
+ === 상관 관계 ===
+ Pclass 
+ Sex
+ Age
+ Fare
+ Embarked
+ === null 값 === null 값 있을땐 for 문 돌리면 에러걸림
  Age      177
  Cabin     687
  Embarked  2
@@ -32,6 +41,7 @@ class TitanicModel(object):
         this.context = './data/'
         this.fname = fname
         return pd.read_csv(this.context + this.fname)
+
     @staticmethod
     def create_train(this) -> object:
         return this.train.drop('Survived', axis = 1) # drop은 dataset 에 있는 train에있는 Suvived열을 지운다
@@ -45,3 +55,42 @@ class TitanicModel(object):
             this.train = this.train.drop(i, axis = 1)
             this.test = this.test.drop(i, axis = 1)
         return this
+    @staticmethod
+    def pclass_ordinal(this) -> object: # 1, 2, 3등칸
+        train = this.train
+        test = this.test
+        return this
+    @staticmethod
+    def sex_norminal(this) -> object: # female -> 1 , male -> 0
+        for i in [this.train, this.test]:
+            i['Gender'] = i['Sex'].map({"male" : 0, "female" : 1}) # mapping 할때 쓰는게 dc임
+        return this
+    @staticmethod
+    def age_ordinal(this) -> object: # 10대, 20대, 30대
+        train = this.train
+        test = this.test
+        return this
+    @staticmethod
+    def fare_ordinal(this) -> object: # 비싼거, 보통, 저렴한 것  4등분 pd.qcut()
+        for i in [this.train, this.test]:
+            i['FareBand'] = pd.qcut(i['Fare'], 4, labels=[1, 2, 3, 4] )
+        return this
+    @staticmethod
+    def embarked_nominal(this) -> object: # 승선항구 S, C, Q
+        this.train = this.train.fillna({"Embarked":"S"})    # fillna 는 null 값을 임의로 넣으라는 뜻
+        this.test = this.test.fillna({"Embarked": "S"})
+        for i in [this.train, this.test]:
+            i['Embarked'] = i['Embarked'].map({"S": 1, "C" : 2, "Q" : 3}) # 같은 이름을 넣으면 원래 이름에 덮어짐 train,test에 스칼라가 추가 되는 것이 아님.
+        return this
+
+if __name__ == '__main__':
+    t = TitanicModel()
+    this = Dataset()
+    this.train = t.new_model('train.csv')
+    this.test = t.new_model('test.csv')
+    this = TitanicModel.sex_norminal(this)
+    this = TitanicModel.fare_ordinal(this)
+    this = TitanicModel.embarked_nominal(this)
+    print(this.train.columns)
+    print(this.train.head())
+    print(this.test.columns)
--- titanic/views.py
+++ titanic/views.py
@@ -14,6 +14,7 @@ class TitanicController(object):
     dataset = Dataset()
     model = TitanicModel()
 
+
     def preprocess(self, train, test) -> object: # 전처리
         model = self.model
         this = self.dataset
@@ -21,6 +22,11 @@ class TitanicController(object):
         this.test = model.new_model(test)
         this.id = this.test['PassengerId']
         # columns 편집과정
+        # this = model.pclass_ordinal(this) 데이터자체가 이미 오디널이다.
+        this = model.sex_norminal(this)
+        this = model.age_ordinal(this)
+        this = model.fare_ordinal(this)
+        this = model.embarked_nominal(this)
         return this
 
     def modeling(self, train, test) -> object: # 모델생성
